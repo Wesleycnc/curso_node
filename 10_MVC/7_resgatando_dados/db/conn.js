37b459da09a3ed06268e977b7de8cead99dec57e
Wesleycnc <wesleycaldas728@gmail.com>
2022-07-26 15:04:31 -0300

    Finalizando MVC

---
+++ 10_MVC/7_resgatando_dados/db/conn.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('nodemvc', 'root', 'wesley08', {
+    host: 'localhost',
+    dialect: 'mysql',
+})
+
+try {
+
+    sequelize.authenticate()
+    console.log('Conectamos ao MySQL!')
+
+} catch(error) {
+    console.log(`Não foi possivel conectar: ${error}`)
+}
+
+module.exports = sequelize
